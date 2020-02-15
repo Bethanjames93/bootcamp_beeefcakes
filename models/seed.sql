76ae6607c31d8bac533ebccf08a494fded5724c0
stuart lamble <lamble.stuart@googlemail.com>
2020-02-15 13:34:36 +0000

    updated schema

--- models/seed.sql
+++ models/seed.sql
@@ -1,2 +1,8 @@
 INSERT INTO comments (user_name, comment, rating) VALUES ("Dave The Drinker", "This was a great place to drink", 5);
 INSERT INTO comments (user_name, comment, rating) VALUES ("Sally the sober", "Not for me", 2);
+
+INSERT INTO breweries (brewery_name, address, city, postal_code) VALUES ("Boddington's Brewery", "", "Manchester", "");
+INSERT INTO breweries (brewery_name, address, city, postal_code) VALUES ("JW Lees and Co", "Middleton Junction", "Manchester", "");
+INSERT INTO breweries (brewery_name, address, city, postal_code) VALUES ("BrewDog", "35 Peter St", "Manchester", "");
+INSERT INTO breweries (brewery_name, address, city, postal_code) VALUES ("Track Brewing Company", "5 Sheffield Street", "Manchester", "");
+INSERT INTO breweries (brewery_name, address, city, postal_code) VALUES ("Marbles Beer", "41 Williamson Street", "Manchester", "");
