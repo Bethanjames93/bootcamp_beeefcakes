3f8e4ab4d466081def9be3a3991c82b742f3ec0d
james davies <james.davies92@icloud.com>
2020-02-22 12:04:43 +0000

    added more css styling and adjusted all html files to make app mobile responsive

--- public/onclickfunctionsforhtml.js
+++ public/onclickfunctionsforhtml.js
@@ -33,3 +33,26 @@ function logInFunc() {
 
     }
   }
+
+  function exit() {
+    const logInButton = document.getElementById("log-in-button");
+    const createAccountButton = document.getElementById("create-account-button");
+    const searchBreweryButton = document.getElementById("search-brewery-button");
+    const searchBarHeader = document.getElementById("search-bar-header");
+    const createAccountForm = document.getElementById("create-account-form");
+    const logInForm = document.getElementById("log-in-form");
+    const formGroup = document.getElementById("form-group");
+
+    if (logInButton.style.display === "none") {
+        logInButton.style.display = "block";
+    } else {
+        logInButton.style.display = "=inline";
+        createAccountButton.style.display = "inline";
+        searchBreweryButton.style.display = "inline";
+        searchBarHeader.style.display = "inline";
+        createAccountForm.style.display = "none";
+        logInForm.style.display = "none";
+        formGroup.style.display = "none";
+
+    }
+}
