a55f30f3aa03430cbabe472732db497073d05bfe
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-02-22 10:46:54 +0000

    revert config.js back to previous

--- config/config.js
+++ config/config.js
@@ -14,16 +14,9 @@ module.exports = {
     "dialect": "mysql",
     "logging": false
   },
-  "productionold": {
+  "production": {
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   },
 
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  }
 }
